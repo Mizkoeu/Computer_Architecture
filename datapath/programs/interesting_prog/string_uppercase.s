100332bbcd1904c38b306076ffd4143e1387e570
Mike Zou <zoutianh@grinnell.edu>
2018-11-30 21:22:07 -0600

    added comments

--- datapath/programs/interesting_prog/string_uppercase.s
+++ datapath/programs/interesting_prog/string_uppercase.s
@@ -8,16 +8,15 @@ main:
 	# set up the constants to be used later
   li $s0, TERM
   li $s1, KB
-  li $sp, STACK_TOP
+  li $sp, STACK_TOP  # initialize the stack pointer at the top
 	li $s2, STACK_TOP  # to implement queue, need a pointer at head to dequeue.
 
 loop_top:
   lw $t0, 0($s1)
-  beq $t0, $zero, loop_top
-  # write to terminal
-  sw $t0, 0($s0)
+  beq $t0, $zero, loop_topi  # if no input, simply keep spin-waiting.
+  sw $t0, 0($s0)      # write to terminal
   li $t1, '\n'
-  beq $t0, $t1, prom_process
+  beq $t0, $t1, prom_process # if newline is read, go print line in uppercase.
 	push $t0            # store into stack pointer pos
   j loop_top
 
@@ -25,22 +24,22 @@ prom_process:
 	beq $s2, $sp, end   # if $s2 moved down below $sp, then end program
 	addi $s2, $s2, -4   # iterate through stack from top to where $sp is.
 	lw $t0, 0($s2)
-  li $t1, 97
-  ble! $t1, $t0, smaller
+  li $t1, 97 					# this is character encoding for 'a'
+  ble! $t1, $t0, smaller # if char is below 'a', print without uppercase
 
 continue:
   sw $t0, 0($s0)      # write the converted char to terminal
   j prom_process      # after which, we go back and continue down the stack
 
 smaller:
-  li $t2, 122
-  bge! $t2, $t0, beupper
-  j continue
+  li $t2, 122					# this is character encoding for 'z'
+  bge! $t2, $t0, beupper # if within a-z, then go convert to upper
+	j continue    			# if char is beyond 'z', print without uppercase
 
 beupper:
   addi $t0, $t0, -32  # convert from lower to upper case here.
-  j continue
+  j continue					# proceed with printing!
 
 end:
-	li $sp, STACK_TOP
+	li $sp, STACK_TOP   # some cleanup work, restore $sp to the top.
 	j HALT
